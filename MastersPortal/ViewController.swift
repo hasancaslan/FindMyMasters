817377e2e90b3d149bb21512644532770a101624
Hasan Can Aslan <aslan4044@gmail.com>
2021-05-09 00:33:16 +0300

    Initial Commit

---
+++ MastersPortal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MastersPortal
+//
+//  Created by HASAN CAN on 9/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
